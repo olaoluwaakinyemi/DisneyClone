c752a7815d7887b52444fb07e600a017862a4294
olaoluwaakinyemi <davidakinyemi8@gmail.com>
2022-12-06 23:15:43 +0100

    ADD: Detail_Page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -10,6 +10,28 @@ function Detail() {
         <ImageTitle>
         <img src='https://wallpapercave.com/wp/wp9532728.jpg' />
         </ImageTitle>
+        <Controls>
+            <PlayButton>
+                <img src='/images/play-icon-black.png'/>
+                <span>PLAY</span>
+            </PlayButton>
+            <TrailerButton>
+                <img src='/images/play-icon-white.png'/>
+                <span>TRAILER</span>
+            </TrailerButton>
+            <AddButton>
+                <span>+</span>
+            </AddButton>
+            <GroupWatchButton>
+                <img src='/images/group-icon.png'/>
+            </GroupWatchButton>
+        </Controls>
+        <SubTitle>
+        2021. PG . 1h 42m
+        </SubTitle>
+        <Description>
+        A Colombian teenage girl has to face the frustration of being the only member of her family without magical powers.
+        </Description>
     </Container>
   )
 }
@@ -48,4 +70,64 @@ const ImageTitle = styled.div`
         height: 100%
         object-fit: contain;
     }
+`
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+`
+const PlayButton = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    padding: 0px 24px;
+    margin-right: 22px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background: rgb(249, 249, 249);
+    border: none;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover{
+        background: rgb(198, 198, 198);
+    }
+`
+const TrailerButton = styled(PlayButton)`
+    background: rgba(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+    text-transform: uppercase;
+`
+const AddButton = styled.button`
+    margin-right: 16px;
+    width: 44px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid white;
+    background-color: rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+    span{
+            font-size: 30px;
+            color: white;
+    }
+`
+const GroupWatchButton = styled(AddButton)`
+    background: rgb(0, 0, 0);
+
+`
+const SubTitle = styled.div`
+    color: rgb(249, 249, 249);
+    font-size: 15px;
+    min-height: 20px;
+    margin-top: 36px;
+`
+const Description = styled.div`
+    line-height: 1.4;
+    font-size: 20px;
+    margin-top: 15px;
+    color: rgb(249, 249, 249);
+
 `
